fceda22f4adeec7b3517871bdb2e4c1784a175e0
Ale <alexcoppe@gmail.com>
2022-11-22 17:47:11 +0100

    Bug correction. CLNSIG and CLNREVSTAT are now shown

--- scripts/update_clinvar_in_vcf.py
+++ scripts/update_clinvar_in_vcf.py
@@ -18,26 +18,24 @@ def create_connection(db_file):
 
 def update_clinvar_column(original_line_from_vcf, field, clinvar_entry):
     clinvar_match = re.search("{}=.+?;".format(field), original_line_from_vcf)
-    d = {"CLNREVSTAT": 7}
+    d = {"CLNREVSTAT": 6, "CLNSIG":5}
     #print(clinvar_entry)
     if clinvar_match:
         start = clinvar_match.start()
         end = clinvar_match.end()
         original_match  = original_line_from_vcf[int(clinvar_match.start()):int(clinvar_match.end())]
-        new_match = clinvar_entry[d.get("CLNREVSTAT")]
+        new_match = clinvar_entry[d.get(field)]
         first_part_original_line_from_vcf = original_line_from_vcf[0:start]
         last_part_original_line_from_vcf = original_line_from_vcf[end:len(original_line_from_vcf)]
         new_line = first_part_original_line_from_vcf + field + "=" + new_match + ";" + last_part_original_line_from_vcf
-        #print("original: ", original_line_from_vcf)
-        #print("new: ", new_line)
         return new_line
 
-def update_variant_from_vcf(original_line_from_vcf, start, end,  clinvar_entry):
-    first_part_original_line_from_vcf = original_line_from_vcf[0:start]
-    last_part_original_line_from_vcf = original_line_from_vcf[end:len(original_line_from_vcf)]
-    new_line = first_part_original_line_from_vcf + "CLNSIG=" + clinvar_entry[6] + ";" + last_part_original_line_from_vcf
-
-    return update_clinvar_column(new_line, "CLNREVSTAT", clinvar_entry)[:-1]
+def update_variant_from_vcf(original_line_from_vcf, clinvar_entry):
+    updated_clnsig =  update_clinvar_column(original_line_from_vcf, "CLNSIG", clinvar_entry)[:-1]
+    #print(updated_clnsig)
+    updated_clnrevstat =  update_clinvar_column(updated_clnsig, "CLNREVSTAT", clinvar_entry)[:-1]
+    #print(updated_CLNREVSTAT)
+    return updated_clnrevstat
 
 def main():
     parser = argparse.ArgumentParser(description="Update CLINVAR data in a VCF")
@@ -68,19 +66,21 @@ def main():
                     clnsig_value = clnsig[:-1].split("=")[1]
 
                     cur = conn.cursor()
-                    query = "SELECT * FROM CLINVAR WHERE CHR == '{}' AND POSITION == {} AND ALTERNATIVE == '{}' AND REFERENCE == '{}' LIMIT 2;".format(chromosome_in_variant[3:], position_in_variant, alternative_in_variant, reference_in_variant)
+                    query = "SELECT * FROM CLINVAR WHERE CHR == '{}' AND POSITION == {} AND ALTERNATIVE == '{}' AND REFERENCE == '{}' LIMIT 1;".format(chromosome_in_variant[3:], position_in_variant, alternative_in_variant, reference_in_variant)
+                    #print(query)
                     cur.execute(query)
                     rows = cur.fetchall()
 
                     if len(rows) == 0:
                         print(line[:-1])
                     else:
+                        #print(rows)
                         # line comes from the studied VCFs
                         # rows come from the Clinvar tab separated file
                         description_from_vcs = line[match.start():match.end()].split("=")[1][:-1]
                         descrition_from_clinvar = rows[0][6]
                         if description_from_vcs != descrition_from_clinvar:
-                            new_variant = update_variant_from_vcf(line, match.start(), match.end(), rows[0])
+                            new_variant = update_variant_from_vcf(line, rows[0])
                             print(new_variant)
             else:
                 print(line[:-1])
